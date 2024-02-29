a5d586e905ddbf16d3ef7c211eff838273281273
VladTer06081963 <patrol200611@ukr.net>
2024-02-29 12:08:36 +0200

    add .gitignore

---
+++ scripts/modalcontroller.js
@@ -0,0 +1,20 @@
+modalController({
+  modal: ".modal1",
+  //   btnOpen: ".section__button2",
+  btnOpen: "#card1 .btnOpen1", // Используем селектор внутри карточки для кнопки открытия модального окна 2
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal2",
+  //   btnOpen: ".section__button2",
+  btnOpen: "#card2 .btnOpen2", // Используем селектор внутри карточки для кнопки открытия модального окна 2
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal3",
+  //   btnOpen: ".section__button3",
+  btnOpen: "#card3 .btnOpen3", // Используем селектор внутри карточки для кнопки открытия модального окна 3
+  btnClose: ".modal__close",
+});
--- scripts/script.js
+++ scripts/script.js
@@ -68,24 +68,3 @@ function fromCenter({ x, y }) {
   );
 }
 //-----------------
-
-modalController({
-  modal: ".modal1",
-  //   btnOpen: ".section__button2",
-  btnOpen: "#card1 .btnOpen1", // Используем селектор внутри карточки для кнопки открытия модального окна 2
-  btnClose: ".modal__close",
-});
-
-modalController({
-  modal: ".modal2",
-  //   btnOpen: ".section__button2",
-  btnOpen: "#card2 .btnOpen2", // Используем селектор внутри карточки для кнопки открытия модального окна 2
-  btnClose: ".modal__close",
-});
-
-modalController({
-  modal: ".modal3",
-  //   btnOpen: ".section__button3",
-  btnOpen: "#card3 .btnOpen3", // Используем селектор внутри карточки для кнопки открытия модального окна 3
-  btnClose: ".modal__close",
-});
